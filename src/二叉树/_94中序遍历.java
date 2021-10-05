4f79c89a85b22926bb1c4bb92621e67830c0fae0
zanyajie <zanyajie@123.com>
2021-10-05 15:09:41 +0800

    my second commit

--- src/二叉树/_94中序遍历.java
+++ src/二叉树/_94中序遍历.java
@@ -12,19 +12,6 @@ import java.util.List;
  * @Date 2021/8/10 20:27
  * Version 1.0
  **/
-/*class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}*/
-
 public class _94中序遍历 {
     /*
     public List<Integer> inorderTraversal(TreeNode root) {//栈
